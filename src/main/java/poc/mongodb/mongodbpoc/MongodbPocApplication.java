fc6ef77abfe5f715e8b5ebe4bd197098dc70c5f1
Ермаков Денис <DEErmakov@lanit.ru>
2023-07-25 16:57:03 +0300

    Empty project

---
+++ src/main/java/poc/mongodb/mongodbpoc/MongodbPocApplication.java
@@ -0,0 +1,12 @@
+package poc.mongodb.mongodbpoc;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MongodbPocApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MongodbPocApplication.class, args);
+    }
+}
---
+++ src/test/java/poc/mongodb/mongodbpoc/MongodbPocApplicationTests.java
@@ -0,0 +1,12 @@
+package poc.mongodb.mongodbpoc;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MongodbPocApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+}
