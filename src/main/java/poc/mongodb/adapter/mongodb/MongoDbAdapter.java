c14f7d8cc8c986e833be4f1e2740cc792966ec7d
Ермаков Денис <DEErmakov@lanit.ru>
2023-07-27 13:14:30 +0300

    Logic, MongoDB configs added

--- src/main/java/poc/mongodb/adapter/mongodb/MongoDbAdapter.java
+++ src/main/java/poc/mongodb/adapter/mongodb/MongoDbAdapter.java
@@ -1,7 +1,38 @@
 package poc.mongodb.adapter.mongodb;
 
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Component;
+import poc.mongodb.adapter.mongodb.repository.PartyRepository;
+import poc.mongodb.app.api.Storage;
+import poc.mongodb.domain.Party;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Optional;
+import java.util.stream.StreamSupport;
+
 /**
  * todo Document type MongoDbAdapter
  */
-public class MongoDbAdapter {
+@Component
+@RequiredArgsConstructor
+public class MongoDbAdapter implements Storage {
+    private final PartyRepository partyRepository;
+
+    @Override
+    public void saveParty(Party party) {
+        partyRepository.save(party);
+    }
+
+    @Override
+    public List<Party> getAllParties() {
+        List<Party> list = new ArrayList<>();
+        partyRepository.findAll().forEach(list::add);
+        return list;
+    }
+
+    @Override
+    public Optional<Party> getParty(Long id) {
+        return partyRepository.findById(id);
+    }
 }
